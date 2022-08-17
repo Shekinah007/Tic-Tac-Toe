247889cf572a951d00d1ec926a339e111eff404d
Shekinah <shalomsheks@gmail.com>
2022-08-17 12:34:49 +0100

    Add modal, overlay

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let cells = document.querySelectorAll(".cell");
-let playerTurn = document.querySelector(".playerTurn");
+const cells = document.querySelectorAll(".cell");
+const resetButton = document.querySelector(".reset");
+const playerTurn = document.querySelector(".playerTurn");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const modalMessage = document.querySelector(".message");
 
 class Player {
   constructor(name, playerSide) {
@@ -16,16 +20,85 @@ class GameState {
     this.player1 = player1;
     this.player2 = player2;
     this.currentPlayer = this.player1;
+    this.boardTrack = ["", "", "", "", "", "", "", "", ""];
+    this.winState = "false";
+  }
+
+  win(currentPlayer, cell) {
+    if (
+      (this.boardTrack[0] == this.boardTrack[1] &&
+        this.boardTrack[0] == this.boardTrack[2] &&
+        this.boardTrack[0] != "") ||
+      (this.boardTrack[0] == this.boardTrack[3] &&
+        this.boardTrack[0] == this.boardTrack[6] &&
+        this.boardTrack[0] != "") ||
+      (this.boardTrack[0] == this.boardTrack[4] &&
+        this.boardTrack[0] == this.boardTrack[8] &&
+        this.boardTrack[0] != "") ||
+      (this.boardTrack[1] == this.boardTrack[4] &&
+        this.boardTrack[1] == this.boardTrack[7] &&
+        this.boardTrack[1] != "") ||
+      (this.boardTrack[3] == this.boardTrack[4] &&
+        this.boardTrack[3] == this.boardTrack[5] &&
+        this.boardTrack[3] != "") ||
+      (this.boardTrack[6] == this.boardTrack[7] &&
+        this.boardTrack[6] == this.boardTrack[8] &&
+        this.boardTrack[6] != "") ||
+      (this.boardTrack[2] == this.boardTrack[4] &&
+        this.boardTrack[2] == this.boardTrack[6] &&
+        this.boardTrack[2] != "") ||
+      (this.boardTrack[2] == this.boardTrack[5] &&
+        this.boardTrack[2] == this.boardTrack[8] &&
+        this.boardTrack[2] != "")
+    ) {
+      this.winState = "true";
+      console.log(currentPlayer.name, " Wins!");
+      modal.classList.add("display");
+      overlay.classList.add("overlay-display");
+      modalMessage.innerText = currentPlayer.name + "  Wins!";
+      // alert(currentPlayer.name + " Wins!");
+    } else if (
+      this.boardTrack[0] != "" &&
+      this.boardTrack[1] != "" &&
+      this.boardTrack[2] != "" &&
+      this.boardTrack[3] != "" &&
+      this.boardTrack[4] != "" &&
+      this.boardTrack[5] != "" &&
+      this.boardTrack[6] != "" &&
+      this.boardTrack[7] != "" &&
+      this.boardTrack[8] != ""
+    ) {
+      this.winState = "draw";
+      console.log(currentPlayer.name, ": Its a draw!");
+      modal.classList.add("display");
+      overlay.classList.add("overlay-display");
+      modalMessage.innerText = "Its a Draw!";
+      // alert(currentPlayer.name + ": Its a draw!");
+    }
+  }
+
+  newGame() {
+    this.winState = "false";
+    this.currentPlayer = this.player1;
+    this.boardTrack = ["", "", "", "", "", "", "", "", ""];
+    cells.forEach((cell) => {
+      cell.innerText = "";
+    });
   }
 
   play() {
     cells.forEach((cell, index) => {
       cell.addEventListener("click", () => {
+        if (cell.innerText != "") return;
+        if (this.winState == "true" || this.winState == "draw") return;
+        console.log(index + 1);
+
+        this.updateGameState(index, this.currentPlayer);
         cell.innerText = this.currentPlayer.playerSide;
+        this.win(this.currentPlayer, cell);
 
         if (this.currentPlayer === player1) {
           this.currentPlayer = this.player2;
-          console.log("First");
           playerTurn.innerText = this.currentPlayer.name;
         } else {
           this.currentPlayer = this.player1;
@@ -35,18 +108,21 @@ class GameState {
     });
   }
 
-  getCurrentPlayer() {
-    console.log(this.currentPlayer);
+  updateGameState(index, currentPlayer) {
+    for (let i = 0; i < this.boardTrack.length; i++) {
+      if (i == index) {
+        this.boardTrack[i] = currentPlayer.playerSide;
+      }
+    }
+    console.log(this.boardTrack);
   }
 }
 
-let game = new GameState(player1, player2);
-game.getCurrentPlayer();
-game.play();
+let roundOne = new GameState(player1, player2);
+roundOne.play();
 
-// cells.forEach((cell, index) => {
-//   cell.addEventListener("click", () => {
-//     cell.innerText = player1.playerSide;
-//     console.log(index);
-//   });
-// });
+resetButton.addEventListener("click", () => {
+  roundOne.newGame();
+  modal.classList.remove("display");
+  overlay.classList.remove("overlay-display");
+});
